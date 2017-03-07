f79af364e1ce5da247d76f725f1c4fd2e83bf17f
genghaode <genghaode@126.com>
2017-03-07 16:43:01 +0800

    完善

--- src/utils/fetchMethod.js
+++ src/utils/fetchMethod.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+let url = ''
+if (__DEV__) {
+  url = 'http://localhost:9090'
+}
+if (__PROD__) {
+  url = 'http://localhost:3000'
+}
+
 export const fetchTest = () => {
-  return axios.get('localhost:9090/api/test')
+  return axios.get(`${url}/api/test`)
 }
